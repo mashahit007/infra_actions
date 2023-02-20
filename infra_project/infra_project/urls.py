f9aee188363e7c86e0599dfc62bd7798674c87d4
Мария Хитрова <mashaxit@yandex.ru>
2023-02-20 13:35:17 +0300

    workflows_flake8

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,7 +1,5 @@
-from django.urls import path, include
 from django.contrib import admin
-
-# app_name = 'infra_app'
+from django.urls import include, path
 
 urlpatterns = [
     path("", include("infra_app.urls", namespace="infra_app")),
